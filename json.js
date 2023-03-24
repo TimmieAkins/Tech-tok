7cf83f6030b6733e8a7052ac46da40cf4d6cd0da
AOT <timmieakinde@gmail.com>
2023-03-24 12:24:18 +0100

    just updated the product file

--- json.js
+++ json.js
@@ -1,3 +1,7 @@
+const allCard = document.querySelector(".all-card");
+const otherCard = document.querySelector(".other-card");
+const highCard = document.querySelector(".high-card");
+
 const data = [
     {
       "id": 1,
@@ -39,8 +43,11 @@ const data = [
       "btn": "add to cart",
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603822/lensabl-5pmpSRctZb0-unsplash-removebg-preview_wxc1r9.png "
     },
+  ];
+
+  const data2 = [
     {
-      "id": 6,
+      "id": 1,
       "name": "Ia Blouse",
       "price": 178.09,
       "des": "Have a glance baring upper garment worn with a saree, lehenga and other ethnic wear.",
@@ -48,7 +55,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603812/eugenivy_now-8YSzM8_Ns4M-unsplash-removebg-preview_lfobn2.png "
     },
     {
-      "id": 7,
+      "id": 2,
       "name": "ferragamo bag",
       "price": 520.90,
       "des": "Expertly crafted from the finest materials, this handbag combines style and functionality.",
@@ -56,7 +63,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603839/kitera-dent-U9OnUX40uUQ-unsplash-removebg-preview_rpodne.png "
     },
     {
-      "id": 8,
+      "id": 3,
       "name": "locket",
       "price": 300,
       "des": "Weather you’re dressing up for a special occasion, these necklace are perfects finishing touch.",
@@ -64,7 +71,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603847/andres-vera-CmmYT6Mm948-unsplash-removebg-preview_o7hdvr.png "
     },
     {
-      "id": 9,
+      "id": 4,
       "name": "Backstay Stiff",
       "price": 800,
       "des": "A stay leading aft from the upper part of a mast to the deck or stern.",
@@ -72,15 +79,18 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603808/anubhav-arora-g1vk_Bef2Xk-unsplash-removebg-preview_nzgauv.png "
     },
     {
-      "id": 10,
+      "id": 5,
       "name": "Loafer Heel",
       "price": 560,
       "des": "Take your style to the next level with this sleek and stylish heel shoes.",
       "btn": "add to cart",
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603865/alex-hudson-Plg3KC7cV0o-unsplash-removebg-preview_zrhegl.png "
     },
+  ];
+
+  const data3 = [
     {
-      "id": 11,
+      "id": 1,
       "name": "All Stars",
       "price": 520.90,
       "des": "these shoe consists of a stitched upper portion, and a toe cap and outsole usually made of rubber.",
@@ -88,7 +98,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603812/huyen-nguy-IjzhMi4Cw3w-unsplash-removebg-preview_uhak2n.png "
     },
     {
-      "id": 12,
+      "id": 2,
       "name": "Patent leather",
       "price": 650,
       "des": " Explore our high-gloss, grain-free leather, which has been gloss-finished on the surface.",
@@ -96,7 +106,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603824/kier-in-sight-b4Xk6bzgQWU-unsplash-removebg-preview_cjglav.png "
     },
     {
-      "id": 13,
+      "id": 3,
       "name": "Beach Shoes",
       "price": 980,
       "des": "These shoes are the perfect finishing touch, with a range of colors and sizes to choose from.",
@@ -104,7 +114,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603838/raoul-croes-94KcLYsihLY-unsplash-removebg-preview_qteskd.png "
     },
     {
-      "id": 14,
+      "id": 4,
       "name": "The Fila Hover",
       "price": 1280,
       "des": "A pair of Fila shoe that combine style, comfort and durability in one.",
@@ -112,7 +122,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603836/tom-radetzki-SzVXG6FFh0Q-unsplash-removebg-preview_wmkwh3.png "
     },
     {
-      "id": 15,
+      "id": 5,
       "name": "Nike loafer",
       "price": 1320,
       "des": "These cushioned and insole and padded collar provide superior comfort and support for all-day wear",
@@ -120,7 +130,7 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603808/revolt-164_6wVEHfI-unsplash-removebg-preview_zruqkx.png "
     },
     {
-      "id": 16,
+      "id": 6,
       "name": "Adidas pair ",
       "price": 2500,
       "des": "These durable rubber outsole delivers excellent traction and grip on a variety surfaces ",
@@ -128,4 +138,130 @@ const data = [
       "img": "https://res.cloudinary.com/dwyccayuh/image/upload/v1677603848/matus-hatala-pFzxaKhdFME-unsplash-removebg-preview_outxtt.png "
     }
   ];
-  console.log(data);
+  
+const product = data.map((item) => {
+
+  // const star = `item-${id}`;
+  return `
+  <div class="product-card">
+      <div class="img-container">
+          <i class="icon fa-regular fa-heart"></i>
+          <img src="${item.img}" alt="" class="product-img">
+      </div>
+      <div class="product-info">
+          <div class="product-name">
+              <h3>${item.name}</h3>
+              <h4>$<span>${item.price}</span></h4>
+          </div>
+          <div class="product-descri">
+              <p>${item.des}</p>
+              <div class="rating">
+                  <div class="star-rating" id="star">
+                      <input type="radio" name="rating1" id="rating1">
+                      <label for="rating1" class="fa fa-star"></label>
+                      <input type="radio" name="rating2" id="rating2">
+                      <label for="rating2" class="fa fa-star"></label>
+                      <input type="radio" name="rating3" id="rating3">
+                      <label for="rating3" class="fa fa-star"></label>
+                      <input type="radio" name="rating4" id="rating4">
+                      <label for="rating4" class="fa fa-star"></label>
+                      <input type="radio" name="rating5" id="rating5">
+                      <label for="rating5" class="fa fa-star"></label>
+                      <h4>(<span>154</span>)</h4>
+                  </div>
+              </div>
+              <button class="add-btn">${item.btn}</button>
+          </div>
+      </div>
+  </div>
+  `
+});
+
+
+allCard.innerHTML = product.join('');
+console.log(allCard);
+
+const otherProducts = data2.map((item, id) => {
+
+  const star = `item-${id}`;
+  return `
+  <div id="${star}" class="product-card">
+      <div class="img-container">
+          <i class=" icon fa-regular fa-heart"></i>
+          <img src="${item.img}" alt="" class="product-img">
+      </div>
+      <div class="product-info">
+          <div class="product-name">
+              <h3>${item.name}</h3>
+              <h4>$<span>${item.price}</span></h4>
+          </div>
+          <div class="product-descri">
+              <p>${item.des}</p>
+              <div class="rating">
+                  <div class="star-rating" id="star">
+                      <input type="radio" name="rating1" id="rating1">
+                      <label for="rating1" class="fa fa-star"></label>
+                      <input type="radio" name="rating2" id="rating2">
+                      <label for="rating2" class="fa fa-star"></label>
+                      <input type="radio" name="rating3" id="rating3">
+                      <label for="rating3" class="fa fa-star"></label>
+                      <input type="radio" name="rating4" id="rating4">
+                      <label for="rating4" class="fa fa-star"></label>
+                      <input type="radio" name="rating5" id="rating5">
+                      <label for="rating5" class="fa fa-star"></label>
+                      <h4>(<span>154</span>)</h4>
+                  </div>
+              </div>
+              <button class="add-btn">${item.btn}</button>
+          </div>
+      </div>
+  </div>
+  `
+});
+
+
+otherCard.innerHTML = otherProducts.join('');
+console.log(otherCard);
+
+const highProducts = data3.map((item, id) => {
+
+  const star = `item-${id}`;
+  return `
+  <div id="${star}" class="product-card">
+      <div class="img-container">
+          <i class="icon fa-regular fa-heart"></i>
+          <img src="${item.img}" alt="" class="product-img">
+      </div>
+      <div class="product-info">
+          <div class="product-name">
+              <h3>${item.name}</h3>
+              <h4>$<span>${item.price}</span></h4>
+          </div>
+          <div class="product-descri">
+              <p>${item.des}</p>
+              <div class="rating">
+                  <div class="star-rating" id="star">
+                      <input type="radio" name="rating1" id="rating1">
+                      <label for="rating1" class="fa fa-star"></label>
+                      <input type="radio" name="rating2" id="rating2">
+                      <label for="rating2" class="fa fa-star"></label>
+                      <input type="radio" name="rating3" id="rating3">
+                      <label for="rating3" class="fa fa-star"></label>
+                      <input type="radio" name="rating4" id="rating4">
+                      <label for="rating4" class="fa fa-star"></label>
+                      <input type="radio" name="rating5" id="rating5">
+                      <label for="rating5" class="fa fa-star"></label>
+                      <h4>(<span>154</span>)</h4>
+                  </div>
+              </div>
+              <button class="add-btn">${item.btn}</button>
+          </div>
+      </div>
+  </div>
+  `
+});
+
+
+highCard.innerHTML = highProducts.join('');
+console.log(highCard);
+
